e51f2ab84d5d55577a904e95bc4316df7bfbd9d6
Yashwant <yashwant.sontakke4@gmail.com>
2026-01-14 02:20:27 +0530

    increase DB pool size to 20 to handle concurrency test load

--- app/core/db.py
+++ app/core/db.py
@@ -10,7 +10,13 @@ load_dotenv()
 DATABASE_URL = f"mysql+pymysql://{quote_plus(os.getenv('DB_USERNAME'))}:{quote_plus(os.getenv('DB_PASSWORD'))}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL, 
+    echo=False, 
+    pool_size=20, 
+    max_overflow=10,
+    pool_pre_ping=True
+)
 
 session = sessionmaker(bind=engine)
 
